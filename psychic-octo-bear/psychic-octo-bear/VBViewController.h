930fe35b62103d74581989fa14bffbc95903ea52
Ariel Rodriguez <ariel@volonbolon.net>
2014-05-31 00:17:16 -0300

    Add Xcode Project

---
+++ psychic-octo-bear/psychic-octo-bear/VBViewController.h
@@ -0,0 +1,13 @@
+//
+//  VBViewController.h
+//  psychic-octo-bear
+//
+//  Created by Ariel Rodriguez on 5/31/14.
+//  Copyright (c) 2014 Ariel Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VBViewController : UIViewController
+
+@end
---
+++ psychic-octo-bear/psychic-octo-bearTests/psychic_octo_bearTests.m
@@ -0,0 +1,34 @@
+//
+//  psychic_octo_bearTests.m
+//  psychic-octo-bearTests
+//
+//  Created by Ariel Rodriguez on 5/31/14.
+//  Copyright (c) 2014 Ariel Rodriguez. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface psychic_octo_bearTests : XCTestCase
+
+@end
+
+@implementation psychic_octo_bearTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
