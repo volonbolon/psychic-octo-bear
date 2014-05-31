087e54059aa5f9a3db341282836b8242e54ab281
Ariel Rodriguez <ariel@volonbolon.net>
2014-05-31 19:42:36 -0300

    Edit gitignore

---
+++ psychic-octo-bear/psychic-octo-bear/VBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VBAppDelegate.h
+//  psychic-octo-bear
+//
+//  Created by Ariel Rodriguez on 5/31/14.
+//  Copyright (c) 2014 Ariel Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
